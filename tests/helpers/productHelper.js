8afb57d8c9d8e172e576003ba01136dbbcf2185e
Nishant Raizaday <nraizaday@gmail.com>
2026-02-27 17:46:21 +0100

    GitHub flows check

--- tests/helpers/productHelper.js
+++ tests/helpers/productHelper.js
@@ -1,10 +1,12 @@
 import ProductsPage from '../../pages/productsPage.js';
 
+//Add all items to cart from products page
 export async function addItemsToCart(page, productNames) {
   const products = new ProductsPage(page);
   await products.addProducts(productNames);
 }
 
+//Add single item to cart from products page
 export async function addSingleItemToCart(page, productName) {
   const products = new ProductsPage(page);
   await products.addProduct(productName);
